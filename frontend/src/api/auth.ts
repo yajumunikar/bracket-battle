e1dcc251f11c6d332515e3d0064c95d7f29947f7
yaju munikar <avatar_yaju@live.com>
2026-06-09 11:45:09 -0400

    chore: add env variable for API URL

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8080/api/v1",
 });
 
 API.interceptors.request.use((config) => {
